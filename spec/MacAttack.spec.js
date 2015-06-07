c98173f4304db87e4d7d4c479a646ec23277527e
Gregory Thompson <GT@Gregorys-MacBook-Pro-2.local>
2015-06-07 19:45:03 -0400

    added endpoint which only validates arguments over entire function endpoint

--- spec/MacAttack.spec.js
+++ spec/MacAttack.spec.js
@@ -3,7 +3,11 @@ var MacAttack = require("../lib/MacAttack")
 var urlMac = MacAttack.createMac("http://macattack.com", "/api/length", "my secret", "(([{\"a\": num}...]) -> num)");
 console.log("urlMac = %j", urlMac);
 
-var valid = MacAttack.validateMac(urlMac, "/api/length", "my secret", [{a: 1}, {a: 2}, {a: 3}], function (arrayObjs) {
+var valid = MacAttack.validateMacFn(urlMac, "/api/length", "my secret", [{a: 1}, {a: 2}, {a: 3}], function (arrayObjs) {
 	return arrayObjs.length;
 });
-console.log("valid = %j", valid);
+console.log("MacAttack.validateMacFn valid = %j", valid);
+
+var valid = MacAttack.validateMac(urlMac, "/api/length", "my secret", [{a: 1}, {a: 2}, {a: 3}]);
+console.log("MacAttack.validateMac valid = %j", valid);
+
