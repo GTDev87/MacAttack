3babf53579c813174a1fd949d9deb175e44a9397
Gregory Thompson <GT@Gregorys-MacBook-Pro-2.local>
2015-06-08 22:40:19 -0400

    added action to macattack

--- spec/MacAttack.spec.js
+++ spec/MacAttack.spec.js
@@ -1,13 +1,13 @@
 var MacAttack = require("../lib/macattack")
 
-var urlMac = MacAttack.createMac("http://macattack.com", "/api/length", "my secret", "(([{\"a\": num}...]) -> num)");
+var urlMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(([{\"a\": num}...]) -> num)");
 console.log("urlMac = %j", urlMac);
 
-var valid = MacAttack.validateMacFunction(urlMac, "/api/length", "my secret", [{a: 1}, {a: 2}, {a: 3}], function (arrayObjs) {
+var valid = MacAttack.validateMacFunction(urlMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}], function (arrayObjs) {
 	return arrayObjs.length;
 });
 console.log("MacAttack.validateMacFunction valid = %j", valid);
 
-valid = MacAttack.validateMac(urlMac, "/api/length", "my secret", [{a: 1}, {a: 2}, {a: 3}]);
+valid = MacAttack.validateMac(urlMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}]);
 console.log("MacAttack.validateMac valid = %j", valid);
 
