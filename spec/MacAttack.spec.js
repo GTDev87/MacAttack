63d65328f02176730e1aa83edd453459a45ce7d2
Gregory Thompson <GT@Gregorys-MacBook-Pro-2.local>
2015-06-09 09:39:07 -0400

    added validation check on mac creation

--- spec/MacAttack.spec.js
+++ spec/MacAttack.spec.js
@@ -1,4 +1,5 @@
-var MacAttack = require("../lib/macattack")
+var MacAttack = require("../lib/macattack");
+var parser = require("../lib/parser/parser");
 
 var urlMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(([{\"a\": num}...]) -> num)");
 console.log("urlMac = %j", urlMac);
@@ -11,8 +12,18 @@ console.log("MacAttack.validateMacFunction valid = %j", valid);
 valid = MacAttack.validateMac(urlMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}]);
 console.log("MacAttack.validateMac valid = %j", valid);
 
-var urlMac2 = MacAttack.createMac("http://macattack.com", "my secret", "/api/create", "POST", "(({\"title\": str}) -> (({}) -> {\"title\": str}))");
-console.log("urlMac = %j", urlMac);
+// parser.parse("(([{\"a\": num}...]) -> num)");
+try{
+	var invalidSchemaMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(([{\"a\": num}...]) -> num");
+	console.log("invalidSchemaMac = %j", invalidSchemaMac);
+}catch (e){
+	console.log("good eror");
+}
 
-var urlMac2 = MacAttack.createMac("http://macattack.com", "my secret", "/api/create", "GET", "(((({}) -> {\"title\": str})) -> {\"title\": str})");
-console.log("urlMac = %j", urlMac);
+
+
+// var urlMac2 = MacAttack.createMac("http://macattack.com", "my secret", "/api/create", "POST", "(({\"title\": str}) -> (({}) -> {\"title\": str}))");
+// console.log("urlMac = %j", urlMac);
+
+// var urlMac2 = MacAttack.createMac("http://macattack.com", "my secret", "/api/create", "GET", "(((({}) -> {\"title\": str})) -> {\"title\": str})");
+// console.log("urlMac = %j", urlMac);
