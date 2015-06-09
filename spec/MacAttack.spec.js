97834e6012182f7a76af2d018845d6e91d2ce080
Gregory Thompson <GT@Gregorys-MacBook-Pro-2.local>
2015-06-09 09:05:52 -0400

    added test to spec file

--- spec/MacAttack.spec.js
+++ spec/MacAttack.spec.js
@@ -11,3 +11,8 @@ console.log("MacAttack.validateMacFunction valid = %j", valid);
 valid = MacAttack.validateMac(urlMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}]);
 console.log("MacAttack.validateMac valid = %j", valid);
 
+var urlMac2 = MacAttack.createMac("http://macattack.com", "my secret", "/api/create", "POST", "(({\"title\": str}) -> (({}) -> {\"title\": str}))");
+console.log("urlMac = %j", urlMac);
+
+var urlMac2 = MacAttack.createMac("http://macattack.com", "my secret", "/api/create", "GET", "(((({}) -> {\"title\": str})) -> {\"title\": str})");
+console.log("urlMac = %j", urlMac);
