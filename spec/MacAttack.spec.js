c66c48a20c8957e9d895cf547b575b794c6f6bc5
Gregory Thompson <GT@Gregorys-MacBook-Pro-2.local>
2015-06-10 00:44:41 -0400

    added hook into rho contract validator to start validating macaroon

--- spec/MacAttack.spec.js
+++ spec/MacAttack.spec.js
@@ -12,6 +12,25 @@ console.log("MacAttack.validateMacFunction valid = %j", valid);
 valid = MacAttack.validateMac(urlMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}]);
 console.log("MacAttack.validateMac valid = %j", valid);
 
+
+
+//////testing inbed macaroons
+
+var urlGetterMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({}) -> num)");
+console.log("urlGetterMac = %j", urlGetterMac);
+
+var urlGetterConsumerMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({\"a\": num, \"b\": (({}) -> num)}) -> num)");
+console.log("urlGetterConsumerMac = %j", urlGetterConsumerMac);
+
+var urlGetterNotConsumerMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({\"a\": num, \"b\": (({}) -> str)}) -> num)");
+console.log("urlGetterNotConsumerMac = %j", urlGetterNotConsumerMac);
+
+valid = MacAttack.validateMac(urlGetterConsumerMac, "my secret", "/api/length", "GET", {a: 1, b: urlGetterMac});
+console.log("MacAttack.validateMac valid = %j", valid);
+
+notValid = MacAttack.validateMac(urlGetterNotConsumerMac, "my secret", "/api/length", "GET", {a: 1, b: urlGetterMac});
+console.log("MacAttack.validateMac notValid = %j", notValid);
+
 // parser.parse("(([{\"a\": num}...]) -> num)");
 try{
 	var invalidSchemaMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(([{\"a\": num}...]) -> num");
