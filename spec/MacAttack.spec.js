18ed3e01f1b7446bfdf16aa460bc410f00dce6b1
Gregory Thompson <gregt87@gmail.com>
2015-10-11 20:01:11 -0400

    do not require full location only require ip and port

--- spec/MacAttack.spec.js
+++ spec/MacAttack.spec.js
@@ -1,56 +1,55 @@
 'use strict';
 
 (function () {
-    describe('createMac', function () {
-    	var MacAttack = require("../lib/macattack");
+  describe('createMac', function () {
+    var MacAttack = require("../lib/macattack");
 
-    	var arrayObjectLengthMac 					= MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(([{\"a\": num}...]) -> num)");
-    	var numGetterMac         					= MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({}) -> num)");
-    	var numGetterConsumerMac 					= MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({\"a\": num, \"b\": (({}) -> num)}) -> num)");
-    	var stringGetterNotConsumerMac 		= MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({\"a\": num, \"b\": (({}) -> str)}) -> num)");
-    	var numGetterReturningMac       	= MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({}) -> (({}) -> num))");
-	    var stringGetterReturningMac    	= MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({}) -> (({}) -> str))");
-	    var numGetterReturningConsumerMac = MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(({\"a\": num, \"b\": (({}) -> (({}) -> num))}) -> num)");
+    var arrayObjectLengthMac          = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(([{\"a\": num}...]) -> num)");
+    var numGetterMac                  = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(({}) -> num)");
+    var numGetterConsumerMac          = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(({\"a\": num, \"b\": (({}) -> num)}) -> num)");
+    var stringGetterNotConsumerMac    = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(({\"a\": num, \"b\": (({}) -> str)}) -> num)");
+    var numGetterReturningMac         = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(({}) -> (({}) -> num))");
+    var stringGetterReturningMac      = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(({}) -> (({}) -> str))");
+    var numGetterReturningConsumerMac = MacAttack.createMac("http://macattack.com", 8080, "my secret", "(({\"a\": num, \"b\": (({}) -> (({}) -> num))}) -> num)");
 
-	    beforeEach(function() {});
+    beforeEach(function() {});
 
-      it('should validate arguments and returns', function () {
-				var valid = MacAttack.validateMacFunction(arrayObjectLengthMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}], function (arrayObjs) { return arrayObjs.length; });
-				expect(valid).toBe(true);
-			});
+    it('should validate arguments and returns', function () {
+      var valid = MacAttack.validateMacFunction(arrayObjectLengthMac, "my secret", [{a: 1}, {a: 2}, {a: 3}], function (arrayObjs) { return arrayObjs.length; });
+      expect(valid).toBe(true);
+    });
 
-			it('should validate only arguments', function () {
-				var valid = MacAttack.validateMac(arrayObjectLengthMac, "my secret", "/api/length", "GET", [{a: 1}, {a: 2}, {a: 3}]);
-				expect(valid).toBe(true);
-			});
+    it('should validate only arguments', function () {
+      var valid = MacAttack.validateMac(arrayObjectLengthMac, "my secret", [{a: 1}, {a: 2}, {a: 3}]);
+      expect(valid).toBe(true);
+    });
 
-			it('should validate to true if proper mac is in signature', function () {
-				var valid = MacAttack.validateMac(numGetterConsumerMac, "my secret", "/api/length", "GET", {a: 1, b: numGetterMac});
-				expect(valid).toBe(true);
-			});
+    it('should validate to true if proper mac is in signature', function () {
+      var valid = MacAttack.validateMac(numGetterConsumerMac, "my secret", {a: 1, b: numGetterMac});
+      expect(valid).toBe(true);
+    });
 
-			it('should not validate to true if wrong signature mac is in signature', function () {
-				var valid = MacAttack.validateMac(stringGetterNotConsumerMac, "my secret", "/api/length", "GET", {a: 1, b: numGetterMac});
-				expect(valid).toBe(false);
+    it('should not validate to true if wrong signature mac is in signature', function () {
+      var valid = MacAttack.validateMac(stringGetterNotConsumerMac, "my secret", {a: 1, b: numGetterMac});
+      expect(valid).toBe(false);
+    });
+      
+    it('should check valid signatures of macaroons in request json', function () {
+      var valid = MacAttack.validateMac(numGetterReturningConsumerMac, "my secret", {a: 1, b: numGetterReturningMac});
+      expect(valid).toBe(true);
+    });
 
-			});
-				
-			it('should check valid signatures of macaroons in request json', function () {
-				var valid = MacAttack.validateMac(numGetterReturningConsumerMac, "my secret", "/api/length", "GET", {a: 1, b: numGetterReturningMac});
-				expect(valid).toBe(true);
-			});
+    it('should check invalid signatures of macaroons in request json', function () {
+      var valid = MacAttack.validateMac(numGetterReturningConsumerMac, "my secret", {a: 1, b: stringGetterReturningMac});
+      expect(valid).toBe(false);
+    });
 
-			it('should check invalid signatures of macaroons in request json', function () {
-				var valid = MacAttack.validateMac(numGetterReturningConsumerMac, "my secret", "/api/length", "GET", {a: 1, b: stringGetterReturningMac});
-				expect(valid).toBe(false);
-			});
-
-			it('should throw error if mac signature is not contructed correctly', function () {
-				var throwing = function() {
-	      	MacAttack.createMac("http://macattack.com", "my secret", "/api/length", "GET", "(([{\"a\": num}...]) -> num");
-		    };
-		    
-		    expect(throwing).toThrow();
-	    });
-		});
+    it('should throw error if mac signature is not contructed correctly', function () {
+      var throwing = function() {
+        MacAttack.createMac("http://macattack.com", "my secret", "(([{\"a\": num}...]) -> num");
+      };
+      
+      expect(throwing).toThrow();
+    });
+  });
 })();
